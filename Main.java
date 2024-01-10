a31343a2ec5909313784d45de9e56d2f4d375fd2
aristote94 <96299619+aristote94@users.noreply.github.com>
2024-01-10 14:44:14 +0100

    test hitbox

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        HitBox a = new HitBox(20,30,3,4);
+        HitBox b = new HitBox(40,12,10,14);
+
+        a.intersect(b);
+
     }
 }
