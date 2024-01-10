ef2ed99c6222a7f35daffbef5d7d1d514160b89f
aristote94 <96299619+aristote94@users.noreply.github.com>
2024-01-10 14:13:35 +0100

    ajout rect

--- HitBox.java
+++ HitBox.java
@@ -2,16 +2,21 @@ public class HitBox {
     private double height, width, x, y;
     private String name;
 
-    public static double HitBox(){
+
+    public HitBox(double height,double width,double x, double y){  // Dispose
+        this.height = height;
+        this.width = width;
+        this.x = x;
+        this.y = y;
+    }
+
+   // boolean intersect( HitBox anthotherHitbox);
+
+    public double getX(){
         return 0;
     }
-    public static double getX(){
+    public double getY(){
         return 0;
-    }
-    public static double getY(){
-        return 0;
-    }
-    public static boolean intersect(){
     }
 
 }
